3e8dd1ea50bae17a5131b686662e5f7688e69a8e
Assanali Issatayev <assanali@rocketfirm.com>
2019-09-16 18:52:28 +0600

    ADDED: showing all for every section on shop page

--- src/App.js
+++ src/App.js
@@ -4,14 +4,14 @@ import { Switch, Route } from 'react-router-dom';
 import './App.css';
 
 import HomePage from './pages/homepage/homepage.component';
-import ShowPage from './pages/shop/show.component';
+import ShopPage from './pages/shop/shop.component';
 
 function App() {
   return (
     <div>
       <Switch>
         <Route exact path='/' component={HomePage} />
-        <Route path='/shop' component={ShowPage} />
+        <Route path='/shop' component={ShopPage} />
       </Switch>
     </div>
   );
